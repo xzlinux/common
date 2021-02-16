bef41fc82908e45eb84efa3830a5d6d38a001b4c
xzlinux <xzlinux@163.com>
2021-02-16 17:28:26 +0800

    add common

---
+++ config.go
@@ -0,0 +1,22 @@
+package common
+
+import (
+	"strconv"
+
+	"github.com/micro/go-micro/v2/config"
+	"github.com/micro/go-plugins/config/source/consul/v2"
+)
+
+func GetConsulConfig(host string, port int64, prefix string) (config.Config, error) {
+	consulSource := consul.NewSource(
+		consul.WithAddress(host+":"+strconv.FormatInt(port, 10)),
+		consul.WithPrefix(prefix),
+		consul.StripPrefix(true),
+	)
+	conf, err := config.NewConfig()
+	if err != nil {
+		return conf, err
+	}
+	err = conf.Load(consulSource)
+	return conf, err
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,9 @@
 module github.com/xzlinux/common
 
 go 1.15
+
+require (
+	github.com/micro/go-micro/v2 v2.9.1 // indirect
+	github.com/micro/go-plugins/config/source/consul/v2 v2.9.1 // indirect
+	gopkg.in/natefinch/lumberjack.v2 v2.0.0 // indirect
+)
